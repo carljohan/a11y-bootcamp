13fc33f45b2009a8c0ce73c2a0606c34b2eb7109
carljohan <carljohan@hey.com>
2023-04-26 10:37:46 -0400

    fix(Input.tsx): a11y improvements

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,36 @@
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface IntegerInputProps {
+  id: string;
+  value: number;
+  onChange: (value: number) => void;
+  className?: string;
+  children: React.ReactNode;
+}
 
-export default function Button({ className, children, ...rest }: Props) {
+export default function Input({ id, value, onChange, className, children, ...props }: IntegerInputProps) {
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    const isValidInteger = /^-?\d*$/.test(inputValue);
+
+    if (isValidInteger) {
+      onChange(parseInt(inputValue, 10) || 0);
+    }
+  };
+  
   return (
     <div className={`mt-2 flex flex-col ${className}`}>
-      <label>
+      <label htmlFor={id}>
         <strong>{children}</strong>
       </label>
       <input
         type="text"
-        className="rounded-sm border border-gray-500 px-5 py-2"
-        {...rest}
+        id={id}
+        value={value.toString()}
+        onChange={handleChange}
+        inputMode="numeric"
+        pattern="-?\d*"
+        {...props}
+        className='rounded-sm border border-gray-500 px-5 py-2'
       />
     </div>
   );
