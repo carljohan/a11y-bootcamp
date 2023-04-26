4c063e0c6b5b8adb6967de1b22dd29e9df3fe4e8
carljohan <carljohan@hey.com>
2023-04-26 10:37:31 -0400

    fix(Modal.tsx): a11y improvements

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,17 +1,56 @@
-import Button from "./Button";
+import React, { useEffect, useRef } from 'react';
 
-interface Props {
+interface ModalProps {
   children: React.ReactNode;
+  open: boolean;
   setOpen: (open: boolean) => void;
 }
 
-export default function Modal({ children, setOpen }: Props) {
+const Modal: React.FC<ModalProps> = ({ children, open, setOpen }) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (open) {
+      dialogRef.current?.focus();
+    }
+  }, [open]);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
+    if (event.key === 'Escape') {
+      handleClose();
+    }
+  };
+
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black">
-      <div className="flex max-w-2xl flex-col items-center bg-white p-4">
-        {children}
-        <Button onClick={() => setOpen(false)}>Close</Button>
-      </div>
-    </div>
+    
+      <dialog
+        ref={dialogRef}
+        className='fixed w-full h-full inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4'
+        style={{ display: open ? 'block' : 'none' }}
+        role='dialog'
+        aria-modal='true'
+        tabIndex={-1}
+        onKeyDown={handleKeyDown}
+        onClick={handleClose}
+      >
+        <div 
+        onClick={(e) => e.stopPropagation()}
+        className='relative mx-auto my-8 flex w-full max-w-md flex-col items-center justify-center rounded bg-white p-8 shadow-lg'>
+          {children}
+          <button
+            className='absolute right-0 top-0 mr-4 mt-4 text-lg text-gray-400 focus:text-gray-800 focus:outline-none'
+            onClick={handleClose}
+            aria-label='Close modal'
+          >
+            &times;
+          </button>
+        </div>
+      </dialog>
   );
-}
+};
+
+export default Modal;
