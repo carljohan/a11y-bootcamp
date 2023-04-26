d3a8d90416270c6e170d8fb771f73480e617af7f
carljohan <carljohan@hey.com>
2023-04-26 10:37:36 -0400

    fix(Button.tsx): a11y improvements

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 export default function Button({ className, children, ...rest }: Props) {
   return (
     <button
-      className={`mt-2 rounded-sm bg-blue-500 p-2 text-gray-300 ${className}`}
+      className={`mt-2 rounded-sm bg-blue-600 text-white p-2 ${className}`}
       {...rest}
     >
       {children}
