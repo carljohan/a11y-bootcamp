31703246f53fe92f2f2b015275f797a37dfdda4e
carljohan <carljohan@hey.com>
2023-04-26 10:36:55 -0400

    fix(Products.tsx): a11y improvements

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import Loading from "../components/Loading";
-import Rating from "../components/Rating";
-import { Product } from "../types/product";
+import { useEffect, useState } from 'react';
+import Loading from '../components/Loading';
+import Rating from '../components/Rating';
+import { Product } from '../types/product';
 
 export default function Products() {
   const [products, setProducts] = useState<Product[]>();
 
   useEffect(() => {
     (async () => {
-      const res = await fetch("https://fakestoreapi.com/products");
+      const res = await fetch('https://fakestoreapi.com/products');
       const p = await res.json();
       setTimeout(() => {
         setProducts(p);
@@ -18,22 +18,21 @@ export default function Products() {
 
   return products ? (
     <div>
-      <h1 className="mt-10 text-center text-3xl font-bold">Products</h1>
-      <div className="my-20 flex flex-row flex-wrap">
+      <h1 className='mt-10 text-center text-3xl font-bold'>Products</h1>
+      <div className='my-20 flex flex-row flex-wrap'>
         {products.map((product) => (
-          <div
-            className="group relative flex w-full flex-col justify-center p-10 sm:w-1/2 md:w-1/3 lg:w-1/4"
+          <a
+            className='group relative flex w-full flex-col justify-center p-10 sm:w-1/2 md:w-1/3 lg:w-1/4'
             key={product.id}
-            onClick={() => {
-              window.location.href = `products/${product.id}`;
-            }}
+            href={`products/${product.id}`}
           >
-            <img src={product.image} />
+            <img loading='lazy' src={product.image} height={250} width={300} />
+
             <Rating rating={product.rating} showLabel={false} />
-            <div className="absolute -bottom-5 left-0 hidden w-full text-center group-hover:block">
+            <div className='w-full text-center opacity-0 transition-opacity group-hover:opacity-100'>
               {product.title}
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>
